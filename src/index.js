26d197484c71ac1c98c34911c2b6be675a812fa5
Andrés Ayala <53452577+Tero-AA@users.noreply.github.com>
2019-11-24 13:25:24 -0500

    Facebook OAuth on server

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import middlewaresConfig from './config/middlewares';
+import './config/db';
+import { CustomerRoutes } from './modules'
+
+const app = express();
+
+middlewaresConfig(app);
+
+
+app.get('/', (req, res) => {
+  res.send('welcome')
+});
+
+app.use('/api/v1/customers', CustomerRoutes)
+
+app.listen(3000, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Listening on port 3000');
+  }
+})
