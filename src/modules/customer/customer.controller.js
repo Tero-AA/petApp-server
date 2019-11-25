cebc9e79622cc17713f7d70ba2a7e60df2c470d9
Andrés Ayala <53452577+Tero-AA@users.noreply.github.com>
2019-11-25 10:16:15 -0500

    Add JWT to OAuth login

--- src/modules/customer/customer.controller.js
+++ src/modules/customer/customer.controller.js
@@ -1,24 +1,38 @@
-import * as Yup from "yup";
+import * as Yup from 'yup';
 
-import { PROVIDER_ENUM } from "./customer.model";
-import { AuthProvider } from '../../services/authProvider'
+import { PROVIDER_ENUM } from './customer.model';
+import { AuthProvider } from '../../services/authProvider';
+import { getOrCreateCustomer } from './customer';
+import { AuthServices } from '../../services/Auth';
 
 export const create = async (req, res) => {
   const { token, provider } = req.body;
 
   const bodySchema = Yup.object().shape({
     token: Yup.string().required(),
-    provider: Yup.string().oneOf(PROVIDER_ENUM).required()
+    provider: Yup.string()
+      .oneOf(PROVIDER_ENUM)
+      .required(),
   });
 
   try {
     await bodySchema.validate({ token, provider });
 
-    if (provider === 'FACEBOOK') {
-      const data = await AuthProvider.Facebook.authAsync(token);
+    let data;
 
-      res.status(201).json(data)
+    if (provider === 'FACEBOOK') {
+      data = await AuthProvider.Facebook.authAsync(token);
+    } else if (provider === 'GOOGLE') {
+      data = await AuthProvider.Google.authAsync(token);
+    } else {
+      res.sendStatus(400);
     }
+
+    const customer = await getOrCreateCustomer(data, provider);
+
+    const jwtToken = AuthServices.createToken(customer);
+
+    res.status(200).json({ token: jwtToken });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
